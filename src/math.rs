5b78402232cfd58a375ddc85aa6e8dfdd8d91136
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2023-02-07 19:30:14 +0800

    Little bit of math cleanup.

--- src/math.rs
+++ src/math.rs
@@ -104,35 +104,30 @@ pub(crate) fn opj_uint_subs(mut a: u32, mut b: u32) -> u32 {
 
 #[inline]
 pub(crate) fn opj_lrintf(mut f: f32) -> i64 {
+  // NOTE: `f32::round()` in Rust doesn't produce the same results:
+  // C lrintf:
+  // (39.5000) = 40.0
+  // (38.5000) = 38.0
+  // Rust `round`:
+  // (39.5000) = 40.0
+  // (38.5000) = 39.0
   extern "C" {
     fn lrintf(_: ::libc::c_float) -> ::libc::c_long;
   }
 
   unsafe {
-    lrintf(f)
+    lrintf(f as f32)
   }
 }
 
 #[inline]
 pub(crate) fn opj_int64_clamp(mut a: i64, mut min: i64, mut max: i64) -> i64 {
-  if a < min {
-    return min;
-  }
-  if a > max {
-    return max;
-  }
-  return a;
+  a.clamp(min, max)
 }
 
 #[inline]
 pub(crate) fn opj_int_clamp(mut a: i32, mut min: i32, mut max: i32) -> i32 {
-  if a < min {
-    return min;
-  }
-  if a > max {
-    return max;
-  }
-  return a;
+  a.clamp(min, max)
 }
 
 #[inline]
