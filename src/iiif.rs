3bede293739a54d4cd54d1e35466310a63988dc2
Sophia <136660013+sophmi@users.noreply.github.com>
2024-08-07 03:07:29 +0100

    Add constructors and getters to iiif structs

--- src/iiif.rs
+++ src/iiif.rs
@@ -4,6 +4,7 @@ pub(crate) mod parse;
 
 use std::num::NonZero;
 
+#[derive(Clone, Debug, PartialEq)]
 pub struct ImageRequest {
     identifier: String,
     region: Region,
@@ -13,9 +14,46 @@ pub struct ImageRequest {
     format: Format,
 }
 
+impl ImageRequest {
+    pub fn new<S: Into<String>>(
+        identifier: S,
+        region: Region,
+        size: Size,
+        rotation: Rotation,
+        quality: Quality,
+        format: Format,
+    ) -> ImageRequest {
+        ImageRequest { identifier: identifier.into(), region, size, rotation, quality, format }
+    }
+
+    pub fn identifier(&self) -> &str {
+        &self.identifier
+    }
+
+    pub fn region(&self) -> &Region {
+        &self.region
+    }
+
+    pub fn size(&self) -> Size {
+        self.size
+    }
+
+    pub fn rotation(&self) -> Rotation {
+        self.rotation
+    }
+
+    pub fn quality(&self) -> Quality {
+        self.quality
+    }
+
+    pub fn format(&self) -> Format {
+        self.format
+    }
+}
+
 pub type Dimension = u32;
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum Region {
     Full,
     Square,
@@ -25,22 +63,72 @@ pub enum Region {
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Size {
-    upscale: bool,
     scale: Scale,
+    upscale: bool,
+}
+
+impl Size {
+    pub fn new(scale: Scale) -> Size {
+        Size { upscale: false, scale }
+    }
+
+    pub fn upscaled(scale: Scale) -> Size {
+        Size { upscale: true, scale }
+    }
+
+    pub fn scale(&self) -> Scale {
+        self.scale
+    }
+
+    pub fn upscale(&self) -> bool {
+        self.upscale
+    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Scale {
-    Maximum,
+    Max,
     Percentage(f32),
     Fixed { width: Option<NonZero<Dimension>>, height: Option<NonZero<Dimension>> },
     AspectPreserving { width: NonZero<Dimension>, height: NonZero<Dimension> },
 }
 
+impl Scale {
+    pub fn fixed(width: Option<NonZero<Dimension>>, height: Option<NonZero<Dimension>>) -> Scale {
+        Scale::Fixed { width, height }
+    }
+
+    pub fn fixed_height(height: Option<NonZero<Dimension>>) -> Scale {
+        Scale::Fixed { width: None, height }
+    }
+
+    pub fn fixed_width(width: Option<NonZero<Dimension>>) -> Scale {
+        Scale::Fixed { width, height: None }
+    }
+}
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Rotation {
-    pub mirror: bool,
-    pub degrees: f32,
+    degrees: f32,
+    mirror: bool,
+}
+
+impl Rotation {
+    pub fn new(degrees: f32) -> Rotation {
+        Rotation { mirror: false, degrees }
+    }
+
+    pub fn mirrored(degrees: f32) -> Rotation {
+        Rotation { mirror: true, degrees }
+    }
+
+    pub fn degrees(&self) -> f32 {
+        self.degrees
+    }
+
+    pub fn mirror(&self) -> bool {
+        self.mirror
+    }
 }
 
 #[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
